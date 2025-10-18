08560423715fcf95b22c1ebc913d5569cd84c642
zoozie <topjada2@gmail.com>
2025-10-18 15:11:13 +0300

    adding function

--- Labo_5.cpp
+++ Labo_5.cpp
@@ -6,9 +6,14 @@ int main() {
 //values
 float x, h, a, b, sum, count;
 h=0.8; a=46; b=1.85; sum=0;
-int count = 0;
+int count=0;
 
 float function;
+
+printf("f(x)=(pow(log(b*x), 4) + 0.85) / (pow(a + b * pow(x, 3), 1.0/3) \n");
+printf("x_min=0.4; x_max=6.8; h=0.8. \n");
+
+
 
 
 return 0;
